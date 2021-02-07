fa870ffffec6065f5db96ccd6188a49daa6b2db0
nate-lin <linchunyan335@163.com>
2021-02-07 23:34:38 +0800

    添加自定义异常（习题）

--- 05_ExceptionAndThread/src/com/itheima/demo04/MyException/RegisterException.java
+++ 05_ExceptionAndThread/src/com/itheima/demo04/MyException/RegisterException.java
@@ -1,7 +1,7 @@
 package com.itheima.demo04.MyException;
 
 /**
- * @date Created by 林春燕 on 2021/2/7 20:29
+ * @date Created by 林春燕 on 2021/2/7 21:57
  * @微信公众号 浅悦的编程时光
  * @博客 https://nate-lin.gitee.io
  * @GitHub https://github.com/nate-lin
@@ -20,16 +20,21 @@ package com.itheima.demo04.MyException;
         2.自定义异常类，必须的继承Exception或者RuntimeException
             继承Exception：那么自定义的异常类就是一个运行期异常，无需处理，交给虚拟机处理（中断处理）
 */
-public class RegisterException extends Exception{
+
+public class RegisterException extends /*Exception*/RuntimeException{
     //添加一个空参数的构造方法
     public RegisterException(){
         super();
     }
-    /*
-        添加一个带异常信息的构造方法
-        查看源码发现，所有的异常类都会一个带有异常信息的构造方法，方法内部会调用父类带异常信息的构造方法，让父类来处理这个异常信息
-    */
+
+/*
+    添加一个带异常信息的构造方法
+    查看源码发现，所有的异常类都会一个带有异常信息的构造方法，方法内部会调用父类带异常信息的构造方法，让父类来处理这个异常信息
+*/
     public RegisterException(String message){
-        //super(message);
+        super(message);
     }
 }
+
+
+
